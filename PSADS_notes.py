c7f2ff1d4658d9e609999ec765446b1db7fb25c2
forestie9 <forestie9@users.noreply.github.com>
2018-08-08 23:10:27 -0500

    Add files via upload
    notes for the ebook "Problem solving with algorithms and data structures"

---
+++ PSADS_notes.py
@@ -0,0 +1,47 @@
+''' notes for the ebook "Problem solving with algorithms and data structures"'''
+
+# Chapter 1
+# Functions
+# Fraction Functions
+# Euclid’s Algorithm states that the greatest common divisor of two integers m and n is n if n divides m evenly. 
+
+def gcd(m,n):
+    while m%n != 0:
+        oldm = m
+        oldn = n
+
+        m = oldn
+        n = oldm%oldn
+    return n
+
+class Fraction:
+     def __init__(self,top,bottom):
+         self.num = top
+         self.den = bottom
+
+     def __str__(self):
+         return str(self.num)+"/"+str(self.den)
+
+     def show(self):
+         print(self.num,"/",self.den)
+
+     def __add__(self,otherfraction):
+         newnum = self.num*otherfraction.den + \
+                      self.den*otherfraction.num
+         newden = self.den * otherfraction.den
+         common = gcd(newnum,newden)
+         return Fraction(newnum//common,newden//common)
+
+     def __eq__(self, other):
+         firstnum = self.num * other.den
+         secondnum = other.num * self.den
+
+         return firstnum == secondnum
+
+x = Fraction(1,2)
+y = Fraction(2,3)
+print(x+y)
+print(x == y)
+
+
+
